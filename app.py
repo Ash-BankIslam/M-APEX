152ca534354ce8897b5982a4631c8b8721fba4a4
Ash-BankIslam <asyrul.bankislam@gmail.com>
2026-09-24 22:47:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ _, col_center_img, _ = st.columns([1, 1, 1])
 with col_center_img:
     st.image("UiTM FSKM.png", width=350)
 
-st.markdown("<h1 style='text-align: center;'>☁️ M-APEX: Malaysian Haze Analysis and Prediction System</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>☁️ M-APEX: Malaysia Air Pollution Extreme Prediction</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Monitor current environmental conditions and 24-hour particulate matter predictions.</p>", unsafe_allow_html=True)
 
 st.markdown("---")
